55c89841af70127a117457d4b393fef36b2a7601
Runxuan Shu <runxuan.shu@unity.cn>
2023-12-29 15:16:38 +0800

    Refactor vulkan surface

--- src/Rendering/Vulkan/VulkanSurface.cpp
+++ src/Rendering/Vulkan/VulkanSurface.cpp
@@ -3,6 +3,8 @@
 #include "VulkanSurface.h"
 #include "VulkanDevice.h"
 
+#include "Windowing/Window.h"
+
 #if PLATFORM_WIN
     #include "Platform/Windows/Win32Window.h"
 #elif PLATFORM_LINUX
@@ -18,45 +20,47 @@
 namespace gore
 {
 
-VulkanSurface::VulkanSurface(VulkanDevice* device, void* nativeWindowHandle) :
+VulkanSurface::VulkanSurface(VulkanDevice* device, Window* window) :
+    m_Window(window),
     m_Device(device),
     m_Surface(VK_NULL_HANDLE)
 {
     VkInstance instance = m_Device->GetInstance()->Get();
+    void* nativeWindowHandle = m_Window->GetNativeHandle();
 #ifdef VK_KHR_win32_surface
 
-    Win32Window* window = reinterpret_cast<Win32Window*>(nativeWindowHandle);
+    Win32Window* win32Window = reinterpret_cast<Win32Window*>(nativeWindowHandle);
     VkWin32SurfaceCreateInfoKHR surfaceCreateInfo{
         .sType     = VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR,
         .pNext     = VK_NULL_HANDLE,
         .flags     = 0,
-        .hinstance = window->hInstance,
-        .hwnd      = window->hWnd,
+        .hinstance = win32Window->hInstance,
+        .hwnd      = win32Window->hWnd,
     };
 
     VkResult res = vkCreateWin32SurfaceKHR(instance, &surfaceCreateInfo, VK_NULL_HANDLE, &m_Surface);
 
 #elif VK_KHR_xlib_surface
 
-    X11Window* window = reinterpret_cast<X11Window*>(nativeWindowHandle);
+    X11Window* x11Window = reinterpret_cast<X11Window*>(nativeWindowHandle);
     VkXlibSurfaceCreateInfoKHR surfaceCreateInfo{
         .sType  = VK_STRUCTURE_TYPE_XLIB_SURFACE_CREATE_INFO_KHR,
         .pNext  = VK_NULL_HANDLE,
         .flags  = 0,
-        .dpy    = window->display,
-        .window = window->window,
+        .dpy    = x11Window->display,
+        .window = x11Window->window,
     };
 
     VkResult res = vkCreateXlibSurfaceKHR(instance, &surfaceCreateInfo, VK_NULL_HANDLE, &m_Surface);
 
 #elif VK_EXT_metal_surface
 
-    CocoaWindow* window = reinterpret_cast<CocoaWindow*>(nativeWindowHandle);
+    CocoaWindow* cocoaWindow = reinterpret_cast<CocoaWindow*>(nativeWindowHandle);
     VkMetalSurfaceCreateInfoEXT surfaceCreateInfo{
         .sType  = VK_STRUCTURE_TYPE_METAL_SURFACE_CREATE_INFO_EXT,
         .pNext  = VK_NULL_HANDLE,
         .flags  = 0,
-        .pLayer = window->layer,
+        .pLayer = cocoaWindow->layer,
     };
 
     VkResult res = vkCreateMetalSurfaceEXT(instance, &surfaceCreateInfo, VK_NULL_HANDLE, &m_Surface);
--- src/Rendering/Vulkan/VulkanSurface.h
+++ src/Rendering/Vulkan/VulkanSurface.h
@@ -5,17 +5,20 @@
 namespace gore
 {
 
+class Window;
 class VulkanDevice;
 
 class VulkanSurface
 {
 public:
-    VulkanSurface(VulkanDevice* device, void* nativeWindowHandle);
+    VulkanSurface(VulkanDevice* device, Window* window);
     ~VulkanSurface();
 
     [[nodiscard]] VkSurfaceKHR Get() const { return m_Surface; }
 
 private:
+    Window* m_Window;
+
     VulkanDevice* m_Device;
 
     VkSurfaceKHR m_Surface;
