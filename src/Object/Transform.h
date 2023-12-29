016fb2d639745213f22c0b586f91bf4cdfe65131
marshallw906 <marshallw906@gmail.com>
2023-12-30 00:34:49 +0800

    Ensure "Math/Math.h" is included at LAST to avoid conflicts between sal.h & GNU STL

--- src/Object/Transform.h
+++ src/Object/Transform.h
@@ -1,9 +1,17 @@
 #pragma once
 #include "Export.h"
 
-#include "Math/Math.h"
-
 #include "Component.h"
+
+#include "Core/Log.h"
+
+
+// THIS IS IMPORTANT: DO NOT INCLUDE ANYTHING (OTHER THAN this Math.h) FROM THE ENGINE BELOW THIS LINE
+// This is because the SAL annotation preprocessor (sal.h) provided by Microsft::DirectXMath conflicts with GNU C++ STL
+// and the workaround is to include system headers BEFORE including DirectXMath headers
+// See https://github.com/Microsoft/DirectXMath?tab=readme-ov-file#compiler-support for details
+// Also, this has already an issue before: https://github.com/microsoft/vcpkg/issues/26702 
+#include "Math/Math.h"
 
 namespace gore
 {
