b2805f42acf62c0bb300cc328ba499ee3bfa821c
MarshallW906 <marshallw906@gmail.com>
2024-01-01 23:47:54 +0800

    Implement Quaternion SIMD based constructors and CreateFromYawPitchRoll()

--- src/Math/Quaternion.cpp
+++ src/Math/Quaternion.cpp
@@ -1,1 +1,23 @@
 #include "Quaternion.h"
+
+namespace gore
+{
+
+Quaternion::Quaternion(const ValueType& F) noexcept :
+    Quaternion()
+{
+    rtm::quat_store(F, reinterpret_cast<float*>(this));
+}
+
+Quaternion::Quaternion(ValueType&& F) noexcept :
+    Quaternion()
+{
+    rtm::quat_store(F, reinterpret_cast<float*>(this));
+}
+
+Quaternion Quaternion::CreateFromYawPitchRoll(float yaw, float pitch, float roll) noexcept
+{
+    return Quaternion(rtm::quat_from_euler(pitch, yaw, roll));
+}
+
+} // namespace gore
