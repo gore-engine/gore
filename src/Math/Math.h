0fdefa52bc58da8ff23f4f05b813070a2a31b594
MarshallW906 <marshallw906@gmail.com>
2024-01-01 19:31:01 +0800

    remove DirectXMath

--- src/Math/Math.h
+++ src/Math/Math.h
@@ -1,20 +1,6 @@
 #pragma once
 
-#include "SimpleMath.h"
-
 namespace gore
 {
-
-using Rect       = DirectX::SimpleMath::Rect;
-using Rectangle  = DirectX::SimpleMath::Rectangle;
-using Vector2    = DirectX::SimpleMath::Vector2;
-using Vector3    = DirectX::SimpleMath::Vector3;
-using Vector4    = DirectX::SimpleMath::Vector4;
-using Matrix4x4  = DirectX::SimpleMath::Matrix4x4;
-using Plane      = DirectX::SimpleMath::Plane;
-using Quaternion = DirectX::SimpleMath::Quaternion;
-using Color      = DirectX::SimpleMath::Color;
-using Ray        = DirectX::SimpleMath::Ray;
-using Viewport   = DirectX::SimpleMath::Viewport;
 
 } // namespace gore
