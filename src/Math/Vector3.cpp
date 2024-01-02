01b24da77e47c0b6cda429d10f2bdd48e5855f35
marshallw906 <marshallw906@gmail.com>
2024-01-02 15:30:40 +0800

    Implement conversion from rtm::vector4f to gore::Vector3

--- src/Math/Vector3.cpp
+++ src/Math/Vector3.cpp
@@ -1,1 +1,14 @@
 #include "Vector3.h"
+
+#include "rtm/vector4f.h"
+
+namespace gore
+{
+
+Vector3::operator ValueType() const noexcept
+{
+    return rtm::vector_load3((reinterpret_cast<const float*>(this)));
+}
+
+
+}
