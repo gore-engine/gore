89a7b18aef790f5ce7b9b61f55288781ad00ac82
MarshallW906 <marshallw906@gmail.com>
2024-01-02 23:37:14 +0800

    Add Math Constants file

---
+++ src/Math/Constants.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace gore::math::constants
+{
+
+constexpr float PI = 3.1415926535897932384626433832795f;
+constexpr float PI_2 = 1.5707963267948966192313216916398f;
+constexpr float PI_4 = 0.78539816339744830961566084581988f;
+constexpr float PI_180 = 0.01745329251994329576923690768489f;
+
+constexpr float Epsilon = 1.0E-8f;
+
+}
