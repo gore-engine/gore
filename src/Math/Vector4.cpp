649ea3e7cbee0eaceb38d3c6131d0e7fc0e5d1d0
marshall.wang <marshall.wang@unity.cn>
2024-01-05 15:21:48 +0800

    Add Vector4 => SIMDValueType conversion

--- src/Math/Vector4.cpp
+++ src/Math/Vector4.cpp
@@ -1,11 +1,20 @@
 #include "Vector4.h"
 
+#include "rtm/vector4f.h"
+
 namespace gore
 {
+
+using namespace rtm;
 
 std::ostream& operator<<(std::ostream& os, const Vector4& v) noexcept
 {
     return os << "Vector4(" << v.x << ", " << v.y << ", " << v.z << ", " << v.w << ")";
+}
+
+Vector4::operator SIMDValueType() const noexcept
+{
+    return vector_load((reinterpret_cast<const float*>(this)));
 }
 
 } // namespace gore
