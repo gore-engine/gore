3bfc75bc054a2739a1e1f8171608892c104d7386
yao.liu <top61ly@gmail.com>
2024-01-08 23:04:19 +0800

    Add triangle vertex buffer shader

--- src/Shaders/sample/triangle.hlsl
+++ src/Shaders/sample/triangle.hlsl
@@ -1,32 +1,24 @@
-static const float2 vertPos[3] =
+struct Attributes
 {
-    float2( 0.5f,  0.5f),
-    float2( 0.0f, -0.5f),
-    float2(-0.5f,  0.5f)
+    float3 positionOS : POSITION;
+    half4 color : COLOR;
 };
 
-static const float3 vertColor[3] =
-{
-    float3(1.0f, 0.0f, 0.0f),
-    float3(0.0f, 1.0f, 0.0f),
-    float3(0.0f, 0.0f, 1.0f)
-};
-
-struct VertOut
+struct Varyings
 {
     float4 pos : SV_Position;
-    float3 fragColor : COLOR;
+    float3 color : COLOR;
 };
 
-VertOut vs(int vertID : SV_VertexID)
+Varyings vs(Attributes IN)
 {
-    VertOut vertOut;
-    vertOut.pos = float4(vertPos[vertID], 0.0f, 1.0f);
-    vertOut.fragColor = vertColor[vertID];
-    return vertOut;
+    Varyings OUT = (Varyings)0;
+    OUT.pos = float4(IN.positionOS, 1.0f);
+    OUT.color = IN.color.rgb;
+    return OUT;
 }
 
-float4 ps(VertOut vertOut) : SV_Target0
+float4 ps(Varyings IN) : SV_Target0
 {
-    return float4(vertOut.fragColor, 1.0f);
+    return float4(IN.color, 1.0f);
 }
