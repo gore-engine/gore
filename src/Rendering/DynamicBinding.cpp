700fd3e656ab1c1c970965784fa610d6fd18993e
Top61ly <top61ly@gmail.com>
2024-01-31 22:03:11 +0800

    Basic dynamic binding

---
+++ src/Rendering/DynamicBinding.cpp
@@ -0,0 +1,24 @@
+#include "Core/Log.h"
+#include "DynamicBinding.h"
+
+namespace gore
+{
+DynamicBinding::DynamicBinding(ShaderStage stage, uint32_t size, uint8_t* data, uint32_t offset)
+{
+    m_Stage  = stage;
+    m_Size   = size;
+    m_Offset = offset;
+    m_Data.resize(size);
+    memcpy(m_Data.data(), data, size);
+}
+void DynamicBinding::setData(const void* data, uint32_t size, uint32_t offset)
+{
+    if (offset + size > m_Size)
+    {
+        LOG_STREAM(DEBUG) << "Too large data!" <<std::endl;
+        size = m_Size - offset;
+    }
+
+    memcpy(m_Data.data() + offset, data, size);
+}
+} // namespace gore
