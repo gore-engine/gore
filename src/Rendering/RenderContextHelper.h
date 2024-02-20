984ad49557b1a8c0621ed9dfae41f34a8efe9c16
Top61ly <top61ly@gmail.com>
2024-02-20 19:57:53 +0800

    Basic DynamicState

--- src/Rendering/RenderContextHelper.h
+++ src/Rendering/RenderContextHelper.h
@@ -94,4 +94,5 @@ vk::PipelineRasterizationStateCreateInfo GetVkRasterizeState(const GraphicsPipel
 vk::PipelineMultisampleStateCreateInfo GetVkMultisampleState(const GraphicsPipelineDesc& desc);
 vk::PipelineDepthStencilStateCreateInfo GetVkDepthStencilState(const GraphicsPipelineDesc& desc);
 vk::PipelineColorBlendStateCreateInfo GetVkColorBlendState(const GraphicsPipelineDesc& desc);
+vk::PipelineDynamicStateCreateInfo GetVkDynamicState(const GraphicsPipelineDesc& desc);
 } // namespace gore::VulkanHelper
