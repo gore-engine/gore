e8842bcf4994a122de49f845fd5de1cc7fca578f
Top61ly <top61ly@gmail.com>
2024-06-03 23:01:02 +0800

    feat: add texture, buffer and sampler bindings

--- src/Rendering/BindGroup.h
+++ src/Rendering/BindGroup.h
@@ -20,19 +20,47 @@ enum class UpdateFrequency : uint8_t
     Count
 };
 
-/// @brief Buffer handle with byte offset
-struct DynamicBuffer final
+enum TextureSamplerBindingType : uint8_t
 {
+    Separated,
+    Combined,
+    Count
+};
+
+struct TextureBinding final
+{
+    uint32_t binding                     = 0;
+    TextureHandle handle                 = {};
+    TextureUsageBits usage               = TextureUsageBits::Sampled;
+    uint8_t arrayIndex                   = 0;
+    uint8_t mipLevel                     = 0;
+    TextureSamplerBindingType sampleType = TextureSamplerBindingType::Separated;
+    // if sampleType == Combined then this is the sampler binding
+    SamplerHandle samplerHandle          = {};
+};
+
+/// @brief Buffer handle with byte offset
+struct BufferBinding final
+{
+    uint32_t binding    = 0;
     BufferHandle handle = {};
     uint32_t byteOffset = 0;
+};
+
+// FIXME: not a good padding
+struct SamplerBinding final
+{
+    uint32_t binding     = 0;
+    SamplerHandle handle = {};
 };
 
 struct BindGroupDesc final
 {
     const char* debugName               = nullptr;
     UpdateFrequency updateFrequency     = UpdateFrequency::None;
-    std::vector<TextureHandle> textures = {};
-    std::vector<DynamicBuffer> buffers  = {};
+    std::vector<TextureBinding> textures = {};
+    std::vector<BufferBinding> buffers  = {};
+    std::vector<SamplerHandle> samplers = {};
     const BindLayout* bindLayout        = nullptr;
 };
 
