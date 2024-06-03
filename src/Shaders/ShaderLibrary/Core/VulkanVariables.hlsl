9672d720b1548e5e1831971ca4eda655c445f59b
Top61ly <top61ly@gmail.com>
2024-06-03 21:37:18 +0800

    feat: add separate texture and sampler

--- src/Shaders/ShaderLibrary/Core/VulkanVariables.hlsl
+++ src/Shaders/ShaderLibrary/Core/VulkanVariables.hlsl
@@ -1,12 +1,13 @@
 #ifndef VULKAN_VARIABLES
 #define VULKAN_VARIABLES
 
-#define STRUCTUREDBUFFER(BufferName, Format) StructuredBuffer<Format> BufferName
-#define RW_STRUCTUREDBUFFER(BufferName, Format) RWStructuredBuffer<Format> BufferName
+#define HLSL_STRUCTUREDBUFFER(BufferName, Format) StructuredBuffer<Format> BufferName
+#define HLSL_RW_STRUCTUREDBUFFER(BufferName, Format) RWStructuredBuffer<Format> BufferName
 
-#define TEXTURE_2D(TextureName, Format) Texture2D<Format> TextureName
-#define SAMPLER(SamplerName) SamplerState SamplerName
+#define HLSL_TEXTURE_2D(TextureName, Format) Texture2D<Format> TextureName
+#define HLSL_SAMPLER(SamplerName) SamplerState SamplerName
 
 #define SAMPLE_TEXTURE_2D(TextureName, UV) TextureName.Sample(TextureName##Sampler, UV)
+#define SAMPLE_TEXTURE_2D_SAMPLER(TextureName, SamplerName, UV) TextureName.Sample(SamplerName, UV)
 
 #endif
