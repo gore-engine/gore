6298202e05179398e5b7020dc02095b8b2899826
Top61ly <top61ly@gmail.com>
2024-11-03 22:20:48 +0800

    feat: introduce DrawStateMask union for rendering state management

--- src/Rendering/DrawStream/DrawStream.h
+++ src/Rendering/DrawStream/DrawStream.h
@@ -6,17 +6,31 @@
 
 namespace gore::renderer
 {
-
-
-struct RendererSort
+union DrawStateMask
 {
-    // custom compare function
-    static operator()(const Renderer* a, const Renderer* b) const
+    struct
     {
-        return a->GetRenderQueue() < b->GetRenderQueue();
-    }
+        uint32_t pipeline : 1;
+        uint32_t bindgroup0 : 1;
+        uint32_t bindgroup1 : 1;
+        uint32_t bindgroup2 : 1;
+        uint32_t indexBuffer : 1;
+        uint32_t vertexBuffer : 1;
+        uint32_t dynamicBuffer : 1;
+        uint32_t indexOffset : 1;
+        uint32_t vertexOffset : 1;
+        uint32_t instanceOffset : 1;
+        uint32_t instanceCount : 1;
+        uint32_t dynamicBufferOffset : 1;
+
+        uint32_t pack : 20;
+    };
+
+    uint32_t mask;
 };
+static_assert(sizeof(DrawStateMask) == 4, "DrawStateMask should be 4 bytes");
 
-
-
+struct DrawStream
+{
+};
 } // namespace gore::renderer
