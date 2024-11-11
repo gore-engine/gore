7e64c1c5d5472497d293c484df5ec9776da8a5ea
Top61ly <top61ly@gmail.com>
2024-11-11 22:26:09 +0800

    feat: enable Factorial tests in TestExampleTest.cpp

--- src/Test/TestExampleTest.cpp
+++ src/Test/TestExampleTest.cpp
@@ -1,18 +1,18 @@
-// #include "TestPrefix.h"
-// #ifdef ENABLE_TEST
+#include "TestPrefix.h"
+#ifdef ENABLE_TEST
 
-// static int Factorial( int number ) {
-//    return number <= 1 ? number : Factorial( number - 1 ) * number;  // fail
-// }
+static int Factorial( int number ) {
+   return number <= 1 ? number : Factorial( number - 1 ) * number;  // fail
+}
 
-// TEST_CASE( "Factorial of 0 is 1 (fail)", "[single-file]" ) {
-//     REQUIRE( Factorial(0) == 1 );
-// }
+TEST_CASE( "Factorial of 0 is 1 (fail)", "[single-file]" ) {
+    REQUIRE( Factorial(0) == 1 );
+}
 
-// TEST_CASE( "Factorials of 1 and higher are computed (pass)", "[single-file]" ) {
-//     REQUIRE( Factorial(1) == 1 );
-//     REQUIRE( Factorial(2) == 2 );
-//     REQUIRE( Factorial(3) == 6 );
-//     REQUIRE( Factorial(10) == 3628800 );
-// }
-// #endif
+TEST_CASE( "Factorials of 1 and higher are computed (pass)", "[single-file]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
+#endif
