7165ed9c47acc2132c5e398a6605632734f1fd2a
Top61ly <top61ly@gmail.com>
2024-12-23 22:40:33 +0800

    feat: add directional light properties to PerframeData structure

--- sample/Scripts/Rendering/PerframeData.h
+++ sample/Scripts/Rendering/PerframeData.h
@@ -1,8 +1,13 @@
 #pragma once
 
 #include "Math/Matrix4x4.h"
+#include "Math/Vector3.h"
 
 struct PerframeData
 {
     gore::Matrix4x4 vpMatrix;
+
+    gore::Matrix4x4 directionalLightVPMatrix;
+    gore::Vector3 directionalLightColor;
+    float directionalLightIntensity;
 };
